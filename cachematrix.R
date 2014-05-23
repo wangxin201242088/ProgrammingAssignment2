767f2e5d192b0d453d010e06bba16f6541f7886d
wangxin201242088 <b.eizhanyon.g@163.com>
2014-05-23 16:45:48 +0800

    The answer of assignment 2
    makeCacheMatrix and cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -6,15 +6,15 @@
 ##get the value of the mean
 
 
-makeCacheMatrix <- function(x = matrix()) {
-  inverse <- NULL
+makeCacheMatrix<- function(x = numeric()) {
+  m <- NULL
   set <- function(y) {
     x <<- y
-    inverse <<- NULL
-  }##set the matrix
-  get <- function() x ##get the matrix
-  setinverse <- function(solve) inverse <<- solve##set the inversion of the matrix
-  getinverse <- function() inverse##get the inversion of the matrix
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) m <<- inverse
+  getinverse <- function() m
   list(set = set, get = get,
        setinverse = setinverse,
        getinverse = getinverse)
@@ -27,14 +27,13 @@ makeCacheMatrix <- function(x = matrix()) {
 ## the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-  inverse <- x$getinverse()
-  if(!is.null(inverse)) {
+  m <- x$getinverse()
+  if(!is.null(m)) {
     message("getting cached data")
-    return(inverse)
-  }##check if there is the cached data of the inverse matrix, if so, show the
-   ##cached data directly
-  data <- x$get()##if not, get the matrix and solve it to get the inverse matrix.
-  inverse <- solve(data)
-  x$setinverse(inverse)
-  inverse ## Return a matrix that is the inverse of 'x'
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data)
+  x$setinverse(m)
+  m
 }
